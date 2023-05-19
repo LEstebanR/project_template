4278ef51aef0a2f2c5d45bad546fafc7619f5043
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-05-19 10:14:33 -0500

    add layout

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,8 @@
-describe('Test', () => {
-  it('should pass', () => {
-    expect(true).to.equal(true)
+// This is just for testing purposes.  Please remove this file when you start writing your own tests.
+
+describe('Home', () => {
+  it('should render "Work in progress...."', () => {
+    cy.visit('/')
+    cy.get('p').should('contain', 'Work in progress...')
   })
 })
